e85f548d13adba1bb48cb857699ec6f7109972fa
santitee <santitee@gmail.com>
2019-11-21 11:40:08 +0700

    Initial commit Write Get and POST API

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express()
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Get - Retrieve data from server
+app.get("/",  (req, res) => {
+    res.json("Hello amazon clone");
+});
+
+// POST - send data frome frontend to backend
+app.post("/", (req, res) => {
+    console.log(req.body.name);
+})
+
+app.listen(3000, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening in PORT", 3000);
+    }
+});
